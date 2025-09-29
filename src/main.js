ab49eac0e6a8436522a54c6265312586fa5f9436
John <john.sarmiento452@gmail.com>
2025-09-29 19:45:20 +0800

    initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "./style.css";
+import Typed from "typed.js";
+
+let typed = new Typed(".text_intro", {
+  strings: ["John Sarmiento", "a Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+  showCursor: false,
+});
